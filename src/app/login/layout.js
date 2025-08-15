228bd6c84e758107b041c8a85dd448d53c22453a
Anass Soukrat <anassookrat@gmail.com>
2025-08-16 00:09:28 +0100

    test

---
+++ src/app/login/layout.js
@@ -0,0 +1,18 @@
+import { getDictionary } from "@/app/[lang]/dictionaries";
+import Login from "@/pages/Login/Login";
+
+export async function generateMetadata({ params }) {
+  const { lang } = await params;
+  const dict = await getDictionary(lang);
+
+  return {
+    title: dict.HELMET.HOME_PAGE.TITLE,
+    description: dict.HELMET.HOME_PAGE.DESCRIPTION,
+  };
+}
+
+export default async function RootLayout({ params }) {
+  const { lang } = await params;
+  const dict = await getDictionary(lang);
+  return <Login dict={dict} />;
+}
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,18 +1,4 @@
-import Login from "@/pages/Login/Login";
-import { getDictionary } from "../[lang]/dictionaries";
-
-export async function generateMetadata({ params }) {
-  const { lang } = await params;
-  const dict = await getDictionary(lang);
-
-  return {
-    title: dict.LOGIN.TITLE,
-    description: dict.LOGIN.DESCRIPTION,
-  };
-}
-
-export default async function LoginPage({ params }) {
-  const { lang } = await params;
-  const dict = await getDictionary(lang);
-  return <Login dict={dict} />;
+"use client";
+export default async function LoginPage({ children }) {
+  return <div>{children}</div>;
 }
